49db95b05cf54b11f20d18b4a1d776d175c03b63
Mario Gonzalez <mayitogl83@gmail.com>
2018-08-16 11:19:10 -0500

    Add fix for properly tag of restaurant name

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt', 'restaurant-image')
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
